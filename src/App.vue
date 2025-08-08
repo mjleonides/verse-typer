627aef906087461ecfa948b10f2027dc35969ac6
Mike Leonides <mike@leonides.dev>
2025-08-08 14:44:25 -0500

    Add documentation

--- src/App.vue
+++ src/App.vue
@@ -29,20 +29,63 @@
 <script setup lang="ts">
 import { ref, computed, watch } from "vue"
 
+/**
+ * Keeps DOM focus on typerInput
+ */
 document.addEventListener("click", () => {
   typerInput.value.focus()
 })
 
+/**
+ * Text input element template ref
+ */
 const typerInput = ref()
+/**
+ * Text input model value
+ */
 const typerInputValue = defineModel()
+/**
+ * Typing challenge string
+ * @type {string}
+ */
 const typerString = ref("Test longer sentence.")
+/**
+ * Ref for keeping track of challenge data (letter success/fail, etc.)
+ */
 const typerData = ref<TyperDataItem[]>([])
+/**
+ * Current typerString char being evaluated
+ */
 const currentItem = computed(() => typerData.value.find((item) => item.isCurrent))
+/**
+ * Index of typerString char being evaluated
+ * @type {number}
+ */
 const currentItemIndex = computed(() => typerData.value.findIndex((item) => item.isCurrent))
+/**
+ * Whether or not challenge is complete
+ * @type {boolean}
+ */
 const typerComplete = computed(() => currentItem.value === undefined)
+/**
+ * Timestamp of first correct keystroke
+ * @type {date}
+ */
 const startTime = ref()
+/**
+ * Timestamp of the success of the last char in typerString
+ * @type {date}
+ */
 const endTime = ref()
+/**
+ * Time of completed challenge in seconds
+ * @type {number}
+ */
 const typedTime = computed(() => (endTime.value - startTime.value) / 1000)
+/**
+ * Debug features toggle
+ * @type {boolean}
+ */
 const debug = ref(false);
 
 interface TyperDataItem {
@@ -51,25 +94,38 @@ interface TyperDataItem {
   isSuccess: boolean | undefined;
 }
 
+/**
+ * Sets ref for keeping track of challenge data (letter success/fail, etc.)
+ */
 const setData = () => {
   typerData.value = typerString.value.split("").map((char, idx) => ({ char, isCurrent: idx === 0, isSuccess: undefined }))
 
   typerInputValue.value = ""
 }
 
+/**
+ * Will create data object once typerString is set (will be most useful once typerString is dynamic)
+ */
 watch(() => typerString.value, (value) => {
   if (value) {
     setData()
   }
 }, { immediate: true })
 
+/**
+ * Catches challenge end time once completed
+ */
 watch(() => typerComplete.value, (value) => {
   if (value) {
     endTime.value = Date.now()
   }
 })
 
-
+/**
+ * Handle keypresses during challenge
+ *
+ * @param {KeyboardEvent} event
+ */
 const handleKeydown = (event: KeyboardEvent) => {
   const currentItem = typerData.value.find((item) => item.isCurrent)
   const nextItem = typerData.value[currentItemIndex.value + 1]
